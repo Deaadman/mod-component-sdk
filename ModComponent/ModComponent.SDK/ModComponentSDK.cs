6374a51307f0236690bbf6efaa5cce8de13e79d0
Deadman <47807486+Deaadman@users.noreply.github.com>
2024-06-25 20:28:56 +0930

    Updated all data assets to v2.28

--- ModComponent/ModComponent.SDK/ModComponentSDK.cs
+++ ModComponent/ModComponent.SDK/ModComponentSDK.cs
@@ -3,10 +3,10 @@ namespace ModComponent.SDK
     public class ModComponentSDK
     {
         public const string SDK_NAME = "ModComponent SDK";
-        public const string SDK_VERSION = "1.0.1";
+        public const string SDK_VERSION = "1.0.2-DeveloperBuild";
 
-        public const string MODCOMPONENT_VERSION = "6.2.6";
-        public const string TLD_VERSION = "2.27";
+        public const string MODCOMPONENT_VERSION = "6.2.7";
+        public const string TLD_VERSION = "2.28";
 
         public const string MODDERSTOOLBOX_VERSION = "1.0.2";
     }
