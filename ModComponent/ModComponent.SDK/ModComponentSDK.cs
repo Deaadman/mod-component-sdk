de7e45d0c80827cf93f79c29e95ab79dbb96342e
Deadman <47807486+Deaadman@users.noreply.github.com>
2024-06-26 17:27:36 +0930

    Updated TLD Version to 2.29 and MC Version to 6.3.0

--- ModComponent/ModComponent.SDK/ModComponentSDK.cs
+++ ModComponent/ModComponent.SDK/ModComponentSDK.cs
@@ -5,8 +5,8 @@ namespace ModComponent.SDK
         public const string SDK_NAME = "ModComponent SDK";
         public const string SDK_VERSION = "1.0.2-DeveloperBuild";
 
-        public const string MODCOMPONENT_VERSION = "6.2.7";
-        public const string TLD_VERSION = "2.28";
+        public const string MODCOMPONENT_VERSION = "6.3.0";
+        public const string TLD_VERSION = "2.29";
 
         public const string MODDERSTOOLBOX_VERSION = "1.0.2";
     }
