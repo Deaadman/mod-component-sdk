fb9100236b9260a0e9202d2a6d53f640d33e55e6
Deadman <47807486+Deaadman@users.noreply.github.com>
2024-06-27 16:31:26 +0930

    Updated Modders' Gear Toolbox to v1.0.3, and next release!

--- ModComponent/ModComponent.SDK/ModComponentSDK.cs
+++ ModComponent/ModComponent.SDK/ModComponentSDK.cs
@@ -3,11 +3,11 @@ namespace ModComponent.SDK
     public class ModComponentSDK
     {
         public const string SDK_NAME = "ModComponent SDK";
-        public const string SDK_VERSION = "1.0.2-DeveloperBuild";
+        public const string SDK_VERSION = "1.0.2";
 
         public const string MODCOMPONENT_VERSION = "6.3.0";
         public const string TLD_VERSION = "2.29";
 
-        public const string MODDERSTOOLBOX_VERSION = "1.0.2";
+        public const string MODDERSTOOLBOX_VERSION = "1.0.3";
     }
 }
