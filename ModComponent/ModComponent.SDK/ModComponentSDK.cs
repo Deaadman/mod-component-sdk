75748aee179b5d840b88e0d29e0a58e6e11005a7
Deadman <47807486+Deaadman@users.noreply.github.com>
2024-06-28 17:28:35 +0930

    MC v6.3.1 bump

--- ModComponent/ModComponent.SDK/ModComponentSDK.cs
+++ ModComponent/ModComponent.SDK/ModComponentSDK.cs
@@ -5,7 +5,7 @@ namespace ModComponent.SDK
         public const string SDK_NAME = "ModComponent SDK";
         public const string SDK_VERSION = "1.0.2";
 
-        public const string MODCOMPONENT_VERSION = "6.3.0";
+        public const string MODCOMPONENT_VERSION = "6.3.1";
         public const string TLD_VERSION = "2.29";
 
         public const string MODDERSTOOLBOX_VERSION = "1.0.3";
