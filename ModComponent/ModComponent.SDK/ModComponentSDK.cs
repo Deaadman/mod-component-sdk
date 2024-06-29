9d910d24b168abf3365c9c861a36dd4a864f2446
Deadman <47807486+Deaadman@users.noreply.github.com>
2024-06-29 09:58:54 +0930

    The Long Dark v2.30 Version Bump

--- ModComponent/ModComponent.SDK/ModComponentSDK.cs
+++ ModComponent/ModComponent.SDK/ModComponentSDK.cs
@@ -6,7 +6,7 @@ namespace ModComponent.SDK
         public const string SDK_VERSION = "1.0.2";
 
         public const string MODCOMPONENT_VERSION = "6.3.1";
-        public const string TLD_VERSION = "2.29";
+        public const string TLD_VERSION = "2.30";
 
         public const string MODDERSTOOLBOX_VERSION = "1.0.3";
     }
