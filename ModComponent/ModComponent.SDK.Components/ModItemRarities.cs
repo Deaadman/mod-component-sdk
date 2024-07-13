e9756a1174e7e378dc953c307bdf2575956dc484
Deadman <47807486+Deaadman@users.noreply.github.com>
2024-07-13 20:05:17 +0930

    Two of the same prefabs can't be added to more than one / the same rarity.

--- ModComponent/ModComponent.SDK.Components/ModItemRarities.cs
+++ ModComponent/ModComponent.SDK.Components/ModItemRarities.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 namespace ModComponent.SDK.Components
 {
@@ -11,5 +12,33 @@ namespace ModComponent.SDK.Components
         public GameObject[] Epic;
         public GameObject[] Legendary;
         public GameObject[] Mythic;
+        
+        private void OnValidate()
+        {
+            ValidateUniquePrefabs();
+        }
+
+        private void ValidateUniquePrefabs()
+        {
+            var uniquePrefabs = new HashSet<GameObject>();
+
+            CheckAndAddToUniquePrefabs(Common, uniquePrefabs, nameof(Common));
+            CheckAndAddToUniquePrefabs(Uncommon, uniquePrefabs, nameof(Uncommon));
+            CheckAndAddToUniquePrefabs(Rare, uniquePrefabs, nameof(Rare));
+            CheckAndAddToUniquePrefabs(Epic, uniquePrefabs, nameof(Epic));
+            CheckAndAddToUniquePrefabs(Legendary, uniquePrefabs, nameof(Legendary));
+            CheckAndAddToUniquePrefabs(Mythic, uniquePrefabs, nameof(Mythic));
+        }
+
+        private static void CheckAndAddToUniquePrefabs(GameObject[] prefabs, HashSet<GameObject> uniquePrefabs, string rarityName)
+        {
+            if (prefabs == null) return;
+
+            for (var i = 0; i < prefabs.Length; i++)
+            {
+                if (prefabs[i] == null || uniquePrefabs.Add(prefabs[i])) continue;
+                prefabs[i] = null;
+            }
+        }
     }
 }
