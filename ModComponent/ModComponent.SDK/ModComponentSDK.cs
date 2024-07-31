4f28e4ec095f8b8ef0bf791f310c095c1ad689f8
Deadman <47807486+Deaadman@users.noreply.github.com>
2024-07-31 16:23:54 +0930

    Updated version to 1.0.4

--- ModComponent/ModComponent.SDK/ModComponentSDK.cs
+++ ModComponent/ModComponent.SDK/ModComponentSDK.cs
@@ -3,7 +3,7 @@ namespace ModComponent.SDK
     public class ModComponentSDK
     {
         public const string SDK_NAME = "ModComponent SDK";
-        public const string SDK_VERSION = "1.0.3";
+        public const string SDK_VERSION = "1.0.4";
 
         public const string MODCOMPONENT_VERSION = "6.3.1";
         public const string TLD_VERSION = "2.31";
